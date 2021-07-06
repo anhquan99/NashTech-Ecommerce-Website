289c3842e9bde3d49e6122da8e8982540a5fa95e
anhquan99 <anhquanala@gmail.com>
2021-07-06 17:11:58 +0700

    update product page

--- Template/Shop/js/function.js
+++ Template/Shop/js/function.js
@@ -1,20 +1,42 @@
-function openNav() {
-    var shadow = document.getElementById("ShadowLayer");
-    var slideBar = document.getElementById("SlidingNav");
+function openNav(navId) {
+    var slideBar = document.getElementById(navId);
     slideBar.style.zIndex = "1";
     slideBar.style.opacity = "1";
     slideBar.style.transform = "none";
-    slideBar.style.transition = "transform 0.3s ease-in-out 0s;"
-    shadow.style.opacity = "1";
-    shadow.style.pointerEvents = "auto";
+    slideBar.style.transition = "transform 0.3s ease-in-out 0s";
+    if(navId == "SlidingNav"){
+        var shadow = document.getElementById("ShadowLayer");
+        shadow.style.opacity = "1";
+        shadow.style.pointerEvents = "auto";
+    }
+
 }
-function closeNav() {
-    var slideBar = document.getElementById("SlidingNav");
-    var shadow = document.getElementById("ShadowLayer");
+function closeNav(navId) {
+    var slideBar = document.getElementById(navId);
+
     slideBar.style.zIndex = "0";
     slideBar.style.opacity = "0";
     slideBar.style.transform = "translate(100vw, 0px)";
     slideBar.style.transition = "opacity 0s linear 0.4s, transform 0.3s ease-in-out 0s";
-    shadow.style.opacity = "0";
-    shadow.style.pointerEvents = "none";
+    if (navId == "SlidingNav") {
+        var shadow = document.getElementById("ShadowLayer");
+        shadow.style.opacity = "0";
+        shadow.style.pointerEvents = "none";
+    }
+}
+function scrollToDetail() {
+    var elmnt = document.getElementById("ProductDetail");
+    elmnt.scrollIntoView();
+
+  }
+function scrollXLeft(carouselId){
+    var slide = document.getElementById(carouselId);
+    slide.scrollLeft += 300;
+}
+function scrollXRight(carouselId){
+    var slide = document.getElementById(carouselId);
+    slide.scrollLeft -= 300;
+}
+function backToTop(){
+    document.documentElement.scrollTop = 0;
 }
